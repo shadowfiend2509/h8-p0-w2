afa247823edb8c434b7c4ac997de37a2900fbf2e
ericsudhartio <*ericsudhartio2509@gmail.com>
2019-07-31 22:25:09 +0700

    update e7

--- Rabu/exercises7.js
+++ Rabu/exercises7.js
@@ -1,18 +1,33 @@
 //1. Menyusun Barisan Bintang
 
-// var rows1='5'
-// for(var i =1 ;i<=rows1;i++){
-//         console.log('*')
-// }
-
+var rows1=5;
+for(var i =1 ;i<=rows1;i++){
+        console.log('*')
+}
+console.log('\n');
 
 //2. Menyusun Barisan Bintang Dengan Nested Looping
 
-var rows2= '5';
-for(var x='1';x<=rows2;x++){
-    for(var y=1;y<=x;y++){
-        console.log('*')
+var rows2=5;
+var string1 ='';
+for(var a=1;a<=rows2;a++){
+    for(var b=1;b<=rows2;b++){
+        string1 += '*';
     }
-    console.log('\n')
+    string1 += '\n';
 }
+console.log(string1);
+
+//3.Menyusun Barisan Tangga Bintang Dengan Nested Looping
+
+
+var rows3= 6;
+var string = '';
+for(var x=1;x<=rows3;x++){
+    for(var y=1;y<=x;y++){
+        string += '*';
+    }
+    string += '\n';
+}
+console.log(string);
     
