419b60c0f508ebfc342cb2ab23916d6d3024522b
ericsudhartio <*ericsudhartio2509@gmail.com>
2019-07-31 10:43:37 +0700

    update e7

---
+++ Rabu/exercises7.js
@@ -0,0 +1,17 @@
+//1. Menyusun Barisan Bintang
+
+var rows1=1;
+for( var x=1;x<=rows1;x++){
+    for(var j=1;j<=x;j++){
+        console.log('*');
+    }
+}
+
+//2. Menyusun Barisan Bintang Dengan Nested Looping
+
+var rows2=3;
+for(x=1;x<rows2;x++){
+    for(j=rows2;j<=rows2;j++){
+        console.log('*');
+    }
+}
