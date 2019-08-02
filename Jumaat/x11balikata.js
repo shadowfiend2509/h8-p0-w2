85376c0b8041bb0b0d0473c7e0b6ee76bcf89185
ericsudhartio <*ericsudhartio2509@gmail.com>
2019-08-02 17:36:59 +0700

    update

--- Jumaat/x11balikata.js
+++ Jumaat/x11balikata.js
@@ -1,26 +1,13 @@
-
-var kata
-
 function balikKata(kata) {
-    var resultkata=''
-    var lengthresultkata=resultkata.length
-    var lengthkata=kata.length
-
-    for(var i=1 ; i<= lengthkata ; i--){
-        if(lengthresultkata===0){
-            resultkata=kata.charAt(lengthkata -i)
-            lengthresultkata=1
-        }else{
-            resultkata = resultkata + kata.charAt(lengthkata-i)
-        }
-        return resultkata
+    var hasilkata=''
+    for(var i=kata.length ; i>=0 ;i--){
+      console.log(i)
+      hasilkata+=kata[i]
     }
-    }
+    return hasilkata
+}
 
   // TEST CASES
-  console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
-  console.log(balikKata('John Doe')); // eoD nhoJ
-  console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
-  console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
-  console.log(balikKata('Super')); 
+  console.log(balikKata(' halo my name is eric'))
+  console.log(balikKata('Super')); //repus
 
--- Selasa/x4fixed.js
+++ Selasa/x4fixed.js
@@ -4,7 +4,7 @@
 
 
 var tanggal=30;
-var bulan=2;
+var bulan=5;
 var tahun=2019;
 
 if(tanggal<1 || tanggal >31){
@@ -19,18 +19,18 @@ if(tanggal<1 || tanggal >31){
     console.log('Maaf untuk tanggal yang di input melebihi tanggal pada bulan '+bulan+'!');
 }else{
     switch(bulan){
-        case 1: {bulan='Januari';break;}
-        case 2: {bulan='Ferbuari';break;}
-        case 3: {bulan='Maret';break;}
-        case 4: {bulan='April';break;}
-        case 5: {bulan='Mei';break;}
-        case 6: {bulan='Juni';break;}
-        case 7: {bulan='July';break;}
-        case 8: {bulan='Agustus';break;}
-        case 9: {bulan='September';break;}
-        case 10: {bulan='Oktober';break;}
-        case 11: {bulan='November';break;}
-        case 12: {bulan='Desember';break;}
+        case 1: {bulan=' Januari ';break;}
+        case 2: {bulan=' Ferbuari ';break;}
+        case 3: {bulan=' Maret ';break;}
+        case 4: {bulan=' April ';break;}
+        case 5: {bulan=' Mei ';break;}
+        case 6: {bulan=' Juni ';break;}
+        case 7: {bulan=' July ';break;}
+        case 8: {bulan=' Agustus ';break;}
+        case 9: {bulan=' September ';break;}
+        case 10: {bulan=' Oktober ';break;}
+        case 11: {bulan=' November ';break;}
+        case 12: {bulan=' Desember ';break;}
     }
-    console.log(tanggal,bulan,tahun);
+    console.log(tanggal + bulan + tahun);
 }
